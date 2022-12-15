f9ce4670e22dd08ca165998fe46d42bc8bd4816b
Dariya <58099281+direcase@users.noreply.github.com>
2022-12-15 15:13:35 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,4 +1,4 @@
-function bitsInInt(n){
+function reverseBits(n){
     let r="";
     let result=0;
     
@@ -17,4 +17,35 @@ function bitsInInt(n){
        
    console.log(r);
    console.log(result);
+}
+
+function factorial(n){
+    let r=1;
+       for(let i=1;i<n+1;i++){
+           
+       console.log(i)
+           r*=i;
+       }
+       console.log(r)
+}
+
+function getUniqueWords(n){
+    let count=0
+    a=a.split(" ")
+    for(let i=0;i<a.length;i++)
+    {
+    c=0;
+    for(let j=0;j<a.length;j++)
+    {
+        if(a[i]==a[j])
+        {
+        c++
+        }
+    }
+    if(count==1)
+    {
+        console.log(a[i])
+    }else console.log("")
+    
+    }
 }
